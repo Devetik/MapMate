cad1db0da90d6c23983d56577a41a2c7332a8ce6
Devetik <sebastien@cuttat.dev>
2024-11-17 20:35:18 +0100

    Add Localization

---
+++ Localization/localization.lua
@@ -0,0 +1,38 @@
+-- Détecte la langue locale
+local L = {}
+local locale = GetLocale()
+
+-- Traductions en anglais (par défaut)
+L["MapMate"] = "MapMate"
+L["Welcome to MapMate!"] = "Welcome to MapMate!"
+L["Show Guild Member Rank"] = "Show Guild Member Rank"
+L["Show Guild Member Level"] = "Show Guild Member Level"
+L["Icon Lock"] = "Lock Icon"
+L["Invite"] = "Invite"
+L["Wisper"] = "Whisper"
+L["Cancel"] = "Cancel"
+L["Icon Size"] = "Icon Size (%)"
+L["MapMate Parameter"] = "MapMate Parameter"
+L["Edit Parameters"] = "Edit Parameters"
+L["Left Click"] = "Left click to open/close parameters."
+
+-- Traductions en français
+if locale == "frFR" then
+    L["MapMate"] = "MapMate"
+    L["Welcome to MapMate!"] = "Bienvenue sur MapMate !"
+    L["Show Guild Member Rank"] = "Afficher du rang des membres de la guilde"
+    L["Show Guild Member Level"] = "Afficher le niveau des membres de la guilde"
+    L["Icon Lock"] = "Verrouiller l'icône"
+    L["Invite"] = "Inviter"
+    L["Wisper"] = "Chuchoter"
+    L["Cancel"] = "Annuler"
+    L["Icon Size"] = "Taille des icônes (%)"
+    L["MapMate Parameter"] = "Paramètres de MapMate"
+    L["Edit Parameters"] = "Modifier les paramètres"
+    L["Left Click"] = "Clic gauche : Ouvrir/fermer les paramètres."
+end
+
+-- Fonction globale pour accéder aux traductions
+function MapMate_Localize(key)
+    return L[key] or key
+end
